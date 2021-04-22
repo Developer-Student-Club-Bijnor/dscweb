761034fabfaae26d44fedbad8255b40dd1e81136
its_Kumar <49340432+its-Kumar@users.noreply.github.com>
2021-04-22 11:49:57 +0530

    Update (#2)
    * Combined compettions, trainings, workshops into events
    remove: extra apps - compettions, trainings, workshop and dscweb
    added: new app events
    
    * Update members
    remove: old members and added: new members
    remove: static members card added: new dynamic cards
    added: android app link to footer
    added: privacy policy to bottom
    hide social bar in mobile view only
    
    * Fix Social Media GitHub Link
    
    * Adds Social account login
    User can signup or login using social accounts like-
    Facebook
    Google
    
    * fix: the issue of running server at local
    
    * password reset change
    
    * Adds: search backend and improoves text-alignment
    
    * Adds: designing to password reset templates
    
    * Organize code and imports
    Adds meta tags
    Adds missing team member's profile
    remove useless static images
    
    * adds: search frontend
    
    * fixed: font style and sieze of moto text
    changed: slider background
    fixed: search results
    fixed: blog post detail page background size
    
    * Add date widget, BlogPostAdmin, Draft feature to Blog
    
    * Add EventAdmin
    
    * Adds Pagination
    
    * Update members.csv
    
    * Added discord server link
    
    * Bump urllib3 from 1.26.2 to 1.26.3
    
    Bumps [urllib3](https://github.com/urllib3/urllib3) from 1.26.2 to 1.26.3.
    - [Release notes](https://github.com/urllib3/urllib3/releases)
    - [Changelog](https://github.com/urllib3/urllib3/blob/main/CHANGES.rst)
    - [Commits](https://github.com/urllib3/urllib3/compare/1.26.2...1.26.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump django from 3.1.3 to 3.1.6
    
    Bumps [django](https://github.com/django/django) from 3.1.3 to 3.1.6.
    - [Release notes](https://github.com/django/django/releases)
    - [Commits](https://github.com/django/django/compare/3.1.3...3.1.6)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump pillow from 8.0.1 to 8.1.1
    
    Bumps [pillow](https://github.com/python-pillow/Pillow) from 8.0.1 to 8.1.1.
    - [Release notes](https://github.com/python-pillow/Pillow/releases)
    - [Changelog](https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst)
    - [Commits](https://github.com/python-pillow/Pillow/compare/8.0.1...8.1.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump django from 3.1.6 to 3.1.8
    
    Bumps [django](https://github.com/django/django) from 3.1.6 to 3.1.8.
    - [Release notes](https://github.com/django/django/releases)
    - [Commits](https://github.com/django/django/compare/3.1.6...3.1.8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: Vikas Kumar <feel.the.coder@gmail.com>
    Co-authored-by: DURGESH PAL <michaeldurgesh@gmail.com>
    Co-authored-by: Vipul Kumar <66182594+Vipul208@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- dscweb/settings.py
+++ dscweb/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from django.conf.global_settings import (EMAIL_HOST, EMAIL_HOST_PASSWORD,
-                                         EMAIL_HOST_USER, EMAIL_USE_TLS)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,41 +24,48 @@ SECRET_KEY = 'qb^uw=)al7t@$0(6$=v%-1yd=484)abu09rk&n6y31_#mvhu6a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'ckeditor',
-    'ckeditor_uploader',
-    'dscweb',
-    'accounts',
-    'blog',
-    'courses',
-    'competitions',
-    'trainings',
-    'workshops',
-    'storages',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # CKEditor apps
+    "ckeditor",
+    "ckeditor_uploader",
+    # django Storage
+    "storages",
+    # local apps
+    "accounts",
+    "blog",
+    "courses",
+    "events",
+    # allauth apps
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.facebook",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'dscweb.urls'
+ROOT_URLCONF = "dscweb.urls"
 
 TEMPLATES = [
     {
@@ -82,17 +87,25 @@ WSGI_APPLICATION = 'dscweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dscweb',
-        'USER': 'dscwebuser',
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+if DEBUG is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dscweb',
+            'USER': 'dscwebuser',
+            'PASSWORD': os.environ['PASSWORD'],
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -116,14 +129,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -134,49 +151,53 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # static paths
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-#LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR),'static_cdn_test')
+# LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR),'static_cdn_test')
 # STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static')  # LIVE CDN AWS S3
-#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # media paths
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'  # django storage
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"  # django storage
 
 # user redirect
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
-
-# Email Backend
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # CkEditor
-CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_UPLOAD_PATH = "uploads/"
 
-# AWS S3 File storage
+if not DEBUG:
+    # Email Backend
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_PORT = "587"
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    # EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-"""# Secrets
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = 'dscweb-bucket'
+    # AWS S3 File storage
 
-AWS_S3_REGION_NAME = "ap-south-1"
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-"""
+    # Secrets
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "dscweb-bucket"
+
+    AWS_S3_REGION_NAME = "ap-south-1"
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+# Social login Django allauth settings
+SITE_ID = 1
